5a374cf8497775ebbb58ae23f8dcc1956e63d187
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-08 09:02:07 +0200

    added view

--- ComicBookGallery/Controllers/ComicBookController.cs
+++ ComicBookGallery/Controllers/ComicBookController.cs
@@ -10,14 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-            else
-            {
-                return Content("hello from ComicBookController");
-            }
+            return View(); 
         }
 
     }
