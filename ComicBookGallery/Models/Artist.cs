189a3b388edc4886b6322d7cb06597debf0b2e5e
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-08 11:45:58 +0200

    strongly typed view

--- ComicBookGallery/Models/Artist.cs
+++ ComicBookGallery/Models/Artist.cs
@@ -2,6 +2,15 @@
 {
     public class Artist
     {
+
+        public Artist() { }
+
+        public Artist(string role, string name)
+        {
+            Name = name;
+            Role = role;
+        }
+
         public string Name { get; set; }
         public string Role { get; set; }
     }
